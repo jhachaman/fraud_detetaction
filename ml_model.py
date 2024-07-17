e812b0488717105954bc0689af274633ab131366
jhachaman <chamanajha560@gmail.com>
2024-07-18 00:20:39 +0530

    added 3 new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,15 @@
-ml_model.py
+   
+print("adding new ml model")
+
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Model3")
+
+def model4():
+    print("adding code for ML Model4")
