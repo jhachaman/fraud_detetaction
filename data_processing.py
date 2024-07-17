1c0085201bd66e929bc8a79c36ae6260a511bd09
jhachaman <chamanajha560@gmail.com>
2024-07-17 18:13:11 +0530

    adding data processing python code

---
+++ data_processing.py
@@ -0,0 +1,12 @@
+
+print("hello world")
+
+a=3
+b=4
+def sum(a,b):
+  c = a+b
+  return c
+
+print("adding a new function sum which are not working")
+
+
---
+++ python.py
@@ -0,0 +1,13 @@
+
+print("adding a new function sum which are not working")
+
+ a = 3
+ b =4
+
+def sum(a,b):
+    c =a+b
+    return c
+
+print("adding a new function sum which are not working")
+
+
